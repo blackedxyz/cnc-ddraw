5de8cb476165207f25815e020830da9a6cbb8486
FunkyFr3sh <cc.red.alert.1@googlemail.com>
2024-06-02 08:18:35 +0200

    disable debug code

--- src/dd.c
+++ src/dd.c
@@ -908,7 +908,7 @@ HRESULT dd_SetDisplayMode(DWORD dwWidth, DWORD dwHeight, DWORD dwBPP, DWORD dwFl
         g_ddraw.render.opengl_y_align = 0;
     }
 
-    dbg_dump_wnd_styles(real_GetWindowLongA(g_ddraw.hwnd, GWL_STYLE), real_GetWindowLongA(g_ddraw.hwnd, GWL_EXSTYLE));
+    //dbg_dump_wnd_styles(real_GetWindowLongA(g_ddraw.hwnd, GWL_STYLE), real_GetWindowLongA(g_ddraw.hwnd, GWL_EXSTYLE));
     if (g_config.windowed)
     {
         if (g_config.remove_menu && GetMenu(g_ddraw.hwnd))
